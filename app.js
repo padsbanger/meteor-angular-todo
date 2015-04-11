3ba968358355fff060ea48005fffdbd2129df81e
Michał Lach <kontakt@michal-lach.pl>
2015-04-11 13:38:59 +0200

    Basic CRUD, LESS, Bootstrap

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+Todos = new Mongo.Collection('todos');
+
 if (Meteor.isClient) {
   var todo = angular.module('todo', ['angular-meteor']);
+
+  todo.controller('todoListController', ['$scope', '$meteor', function($scope, $meteor) {
+
+    $scope.todos = $meteor.collection(Todos);
+    $scope.addTodo = function(todo) {
+      $scope.todos.save(todo);
+    };
+
+    $scope.remove = function(todo) {
+      $scope.todos.remove(todo);
+    };
+
+    $scope.clear = function() {
+      $scope.todos.remove();
+    };
+
+  }]);
+
 }
