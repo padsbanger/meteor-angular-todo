23646d9faa25249b6b741a13afe4dd8538aded59
Michał Lach <kontakt@michal-lach.pl>
2015-04-12 22:27:49 +0200

    Individual page per todo

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 Todos = new Mongo.Collection('todos');
 
 if (Meteor.isClient) {
-  var todo = angular.module('todo', ['angular-meteor']);
+  var todo = angular.module('todo', ['angular-meteor', 'ui.router']);
+
+  todo.config(['$urlRouterProvider', '$stateProvider', '$locationProvider', function($urlRouterProvider, $stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $stateProvider
+      .state('todos', {
+        url: '/todos',
+        templateUrl: 'todos-list.ng.html',
+        controller: 'todoListController'
+      })
+      .state('todoDetails', {
+        url: '/todos/:todoId',
+        templateUrl: 'todo-details.ng.html',
+        controller: 'todoDetailsController'
+      });
+
+    $urlRouterProvider.otherwise("/todos");
+
+  }]);
 
   todo.controller('todoListController', ['$scope', '$meteor', function($scope, $meteor) {
-
     $scope.todos = $meteor.collection(Todos);
+
     $scope.addTodo = function(todo) {
+      todo.date = new Date();
       $scope.todos.save(todo);
     };
 
@@ -18,6 +38,11 @@ if (Meteor.isClient) {
       $scope.todos.remove();
     };
 
+  }]);
+
+  todo.controller('todoDetailsController', ['$scope', '$stateParams','$meteor', function($scope, $stateParams, $meteor) {
+    $scope.todoId = $stateParams.todoId;
+    $scope.todo = $meteor.object(Todos, $stateParams.todoId);
   }]);
 
 }
